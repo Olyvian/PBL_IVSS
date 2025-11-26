f46a3a61f038ac6a41b6fea0e8f2acde0392f3f1
FaatPrasojo <faatprasojo@gmail.com>
2025-11-26 22:47:27 +0700

    Update Footer

---
+++ includes/footer.php
@@ -0,0 +1,47 @@
+<footer class="footer-polinema">
+    <div class="footer-top">
+        <div class="footer-identitas">
+            <div class="logo-container-footer">
+                <img src="../assets/img/logo_polinema.png" alt="Logo POLINEMA">
+                <div class="text-identitas-footer">
+                    <h3>JURUSAN TEKNOLOGI INFORMASI</h3>
+                    <h2>POLITEKNIK NEGERI MALANG</h2>
+                </div>
+            </div>
+            <div class="alamat-info">
+                <p>BLU POLITEKNIK NEGERI MALANG</p>
+                <p>Jl. Soekarno Hatta No.9, Lowokwaru, Kota Malang</p>
+            </div>
+        </div>
+
+        <div class="social-media">
+            <a href="#"><img src="../assets/img/yt.png" alt="YouTube"></a>
+            <a href="#"><img src="../assets/img/ig.jpeg" alt="Instagram"></a>
+        </div>
+    </div>
+    
+    <div class="footer-bottom-menu">
+        <div class="menu-group">
+            <h4>Tentang JTI</h4>
+            <ul>
+                <li><a href="#">Sejarah</a></li>
+                <li><a href="#">Visi, Misi & Tujuan</a></li>
+                <li><a href="#">Struktur Organisasi</a></li>
+                <li><a href="#">Tenaga Pengajar</a></li>
+                <li><a href="#">Tenaga Kependidikan</a></li>
+                <li><a href="#">Sarana & Prasarana</a></li>
+            </ul>
+        </div>
+
+        <div class="menu-group">
+            <h4>Website Polinema Lainnya</h4>
+            <ul>
+                <li><a href="http://polinema.ac.id">Polinema.ac.id</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+
+    <script src="assets/js/script_main.js"></script> 
+</body>
+</html>
