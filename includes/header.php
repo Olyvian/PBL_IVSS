11b33df9dc0719adbb2fc8c5c3eabc07c4924e86
Olyvian <113090307+Olyvian@users.noreply.github.com>
2025-11-30 00:00:49 +0700

    CSS Fix

--- includes/header.php
+++ includes/header.php
@@ -24,7 +24,7 @@ if (isset($_SESSION['user_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     
-    <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="assets/css/style_profil.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <style>
